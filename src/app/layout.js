02da686001a4036b0eddd6e1943bca029015db2f
shresthyadav618 <manushresth.official24669@gmail.com>
2023-08-15 22:43:19 +0530

    added GoogleOA authentication

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
-import './globals.css'
+import AuthProvider from '@/../components/AuthProvider/AuthProvider'
 import { Inter } from 'next/font/google'
-
+import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,7 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      
+      <body className={inter.className}> <AuthProvider> {children} </AuthProvider> </body>
+     
+      
     </html>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,34 @@
 
+"use client"
+import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+export default function Home(){
 
-export default function Home() {
-  return (
-    <div className="w-screen flex items-center justify-center h-screen">
-      <button className="bg-white p-6 text-black">Login with Google</button>
-    </div>
-  )
+const {data: session , status} = useSession();
+
+const userEmail = session?.user?.email;
+if(status ==="loading"){
+  return  <p>Hang in there </p>
 }
 
+if(status ==="unauthenticated"){
+  return <>
+   <div className="w-screen flex items-center justify-center h-screen">
+        <button className="bg-white p-6 text-black" onClick={()=>{signIn("google")}}>Login with Google</button>
+      </div>
+  </>
+}
+
+if(status==="authenticated"){
+  return <>
+  <p>
+    Signed In as {userEmail}
+  </p>
+  <button onClick={()=>{signOut()}}>Sign Out</button>
+  </>
+}
+
+  return(
+    <div>Please <Link href={'/login'}>Login</Link> </div>
+  )
+}
